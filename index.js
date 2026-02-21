096c650e57fe2d429731b9668a3130a7f69898ac
Muhammad Umar <bc220401146muf@gmail.com>
2026-02-21 14:17:33 +0500

    Changes done

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const app = express();
 app.set( "view engine", "ejs" );
 app.set( "views", "./src/views" );
 
+app.use( express.static( "public/" ) )
 app.use( express.urlencoded( { extended: false } ) );
 app.use( express.json() );
 app.use( "/", hompePageRoute );
